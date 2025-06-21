c46e9e34121be2f9c7ec79565a954a1773404932
Sumon K. Mondol <Sumon@techsflow.com>
2025-06-21 22:07:55 +0600

    Add project files.

---
+++ Inventory/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Inventory.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Category { get; set; }
+
+        [Range(0.01, double.MaxValue)]
+        public decimal Price { get; set; }
+
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+    }
+}
